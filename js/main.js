da09240d1bf398eaefc4642830a69d6dbe6b223b
bannister.nicholas <bannister.nicholas@ansibleww.com>
2018-03-12 15:46:06 +1030

    Will Need to rewrite, As I was using directives

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 'use strict';
 var app = angular.module('aligent', []);
 
+/* When Ready to make a Mobile Version of the site, So view sizes and such are not so cramped.
+angular.module('aligent', []).config(function ($routeProvider) {
+    // Magic sauce, immediate so the value is stored and we don't need to lookup every check
+        var _isNotMobile = (function() {
+            var check = false;
+            (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+            return !check;
+        })();
+    
+        // Swap out different HTML because you want to say, hide a video etc.
+        $routeProvider
+            .when('/', {
+                templateUrl: (_isNotMobile )? 'views/MY_DESKTOP_VIEW.html':'views/m/MY_MOBILE_VIEW.html',
+                controller: (_isNotMobile )?'MyHomeCtrl':'MyMobileCtrl'
+              })
+              .otherwise({
+                redirectTo: '/'
+              });
+    }); */
+
 /// TBP = Time Based Problem, cannot solve given the lack of time to do so.
 
 app.controller('aligentControl', function ($scope, $http, $timeout) {
@@ -8,13 +28,22 @@ app.controller('aligentControl', function ($scope, $http, $timeout) {
     // Set up variables
     var apiKey = 'c18f83196bd4532eead800cd4e38ca1d';
 
-    $scope.testOptions = {
+    $scope.filterSize = 3;
+
+    window.mobilecheck = function () {
+        var check = false;
+        (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+        return check;
+    };
+
+    $scope.abvOptions = {
+        id: "ex2",
         min: 3,
         max: 8,
-        step: 0.5,
+        step: 0.1,
         precision: 1,
         orientation: 'horizontal',  // vertical
-        handle: 'triangle', //'square', 'triangle' or 'custom'
+        handle: 'custom', //'square', 'triangle' or 'custom'
         tooltip: 'show', //'hide','always'
         tooltipseparator: ':',
         tooltipsplit: false,
@@ -24,6 +53,36 @@ app.controller('aligentControl', function ($scope, $http, $timeout) {
         ngDisabled: false,
         reversed: false
     };
+
+    $scope.ibuOptions = {
+        id: "ex2",
+        min: 11,
+        max: 23,
+        step: 0.1,
+        precision: 1,
+        orientation: 'horizontal',  // vertical
+        handle: 'custom', //'square', 'triangle' or 'custom'
+        tooltip: 'show', //'hide','always'
+        tooltipseparator: ':',
+        tooltipsplit: false,
+        enabled: true,
+        naturalarrowkeys: false,
+        range: false,
+        ngDisabled: false,
+        reversed: false
+    };
+
+    $scope.abvValue = {
+        min: 4,
+        max: 7,
+        value: 5
+    };
+
+    $scope.ibuValue = {
+        min: 4,
+        max: 7,
+        value: 5
+    }
 
     // Angular Variables
     $scope.beerData = "Loading";
@@ -77,9 +136,12 @@ app.controller('aligentControl', function ($scope, $http, $timeout) {
 
     //Filters List for the Filters search panel, when API is accessable, will adjust
     $scope.filtersList = [
-
-        { name: "AleCategory", value: 1 }, // TBP would prefer to dynamically create this list
-        { name: "AleStyle", value: 2 }
+        { name: "All", value: 0 },
+        { name: "Beers", value: 1 }, // TBP would prefer to dynamically create this list
+        { name: "Ales", value: 2 },
+        { name: "Stouts", value: 3 },
+        { name: "Test4", value: 4 },
+        { name: "Test5", value: 5 }
     ]
 
     $scope.filterAles = function () {
@@ -95,7 +157,8 @@ app.controller('aligentControl', function ($scope, $http, $timeout) {
             if ($scope.beerList[listId].style.categoryId == $scope.formData.aleStyle) {
                 $scope.beerList[listId].hide = false;
             } else { $scope.beerList[listId].hide = true }
-
+            // All List needs to clear all hide order
+            if ($scope.formData.aleStyle == 0) { $scope.beerList[listId].hide = false; }
             // Does the Beer come inside the parameters of the Slide?
         };
     }
@@ -181,23 +244,5 @@ angular.module('aligent').directive('jsonText', function () {
 // Carosell Code
 
 // Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-    interval: false
-});
 
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
-$('.multi-item-carousel .item').each(function () {
-    var next = $(this).next();
-    if (!next.length) {
-        next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-
-    if (next.next().length > 0) {
-        next.next().children(':first-child').clone().appendTo($(this));
-    } else {
-        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-    }
-});
 
